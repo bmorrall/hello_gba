52abb4f44f6e831a2a8dce4e477c6d03710a2452
Ben Morrall <bemo56@hotmail.com>
2023-05-06 19:18:35 +1000

    Add Hello World text

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,47 @@
 #include "bn_core.h"
+#include "bn_math.h"
+#include "bn_keypad.h"
+#include "bn_display.h"
+#include "bn_sprite_ptr.h"
+#include "bn_bg_palettes.h"
+#include "bn_sprite_text_generator.h"
+
+#include "bn_sprite_items_variable_8x16_font_red.h"
+#include "bn_sprite_items_variable_8x16_font_blue.h"
+#include "bn_sprite_items_variable_8x16_font_yellow.h"
+
+#include "common_variable_8x16_sprite_font.h"
+
+namespace
+{
+    constexpr bn::fixed text_y_inc = 14;
+    constexpr bn::fixed text_y_limit = (bn::display::height() / 2) - text_y_inc;
+
+    void text_hello_world()
+    {
+        bn::sprite_text_generator text_generator(common::variable_8x16_sprite_font);
+        text_generator.set_center_alignment();
+
+        bn::vector<bn::sprite_ptr, 32> text_sprites;
+        text_generator.generate(0, -text_y_limit, "Hello World!!!", text_sprites);
+        text_generator.generate(0, text_y_limit, "github.com/bmorrall", text_sprites);
+
+        while(! bn::keypad::start_pressed())
+        {
+            bn::core::update();
+        }
+    }
+}
 
 int main()
 {
     bn::core::init();
 
+    bn::bg_palettes::set_transparent_color(bn::color(16, 16, 16));
+
     while(true)
     {
+        text_hello_world();
         bn::core::update();
     }
 }
